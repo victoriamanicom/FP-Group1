ebdd4010331572596a8df679197a01bbde67beed
Seabohr <sebspace@hotmail.co.uk>
2021-09-21 16:59:56 +0100

    FP1-1 #floating labels in progress

--- Frontend/fp-group1/src/components/searchBar/SearchBar.jsx
+++ Frontend/fp-group1/src/components/searchBar/SearchBar.jsx
@@ -1,26 +1,36 @@
+import Col from 'react-bootstrap/Col';
 
-
-const SearchBar = ({handleSearch}) => {
-
-    const handleReset = e => {
-        e.preventDefault();
-        e.target.reset();
-    }
+const SearchBar = ({handleSearch, handleReset}) => {
 
     return (
         <>
             <form id="searchBarForm" onSubmit={handleSearch}>
-                <div class="container">
-                    <div class="row">
-                        <div class="col">
-                            <label>Forenames</label>
+                <Col md>
+                    <FloatingLabel controlId="floatingForenames" label="Forenames">
+                        <Form.Control type="text" name="searchForenames"/>
+                    </FloatingLabel>
+                </Col>
+                <Col md>
+                    <FloatingLabel controlId="floatingSurname" label="Surname">
+                        <Form.Control type="text" name="searchSurname"/>
+                    </FloatingLabel>
+                </Col>
+                <Col md>
+                    <FloatingLabel controlId="floatingGender" label="Gender">
+                        <Form.Select>
+                            <option value="Female"></option>
+                            <option value="Male"></option>
+                            <option value="Unknown"></option>
+                        </Form.Select>
+                    </FloatingLabel>
+                </Col>
                             <label>Surname</label>
                             <label class="container">Female
                                 <input type="checkbox"/>
                                 <span class="checkmark"></span>
                             </label>
                             <button type="submit">Search</button>
-                        </div>
+                        
                         <div class="col">
                             <input type="text" name="searchForenames"/>
                             <input type="text" name="searchSurname"/>
@@ -31,7 +41,7 @@ const SearchBar = ({handleSearch}) => {
                             <button onClick={handleReset}>Reset</button>
                         </div>
                     </div>
-                </div>
+                
             </form>
         </>
     )
