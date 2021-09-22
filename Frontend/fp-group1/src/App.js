2e35d1ae26bc34f759051adfd7768084773ddef6
victoriamanicom <victoriamanicom@gmail.com>
2021-09-22 09:58:33 +0100

    FP1-1 #imported bootstrap css and fixed styling issues

--- Frontend/fp-group1/src/App.js
+++ Frontend/fp-group1/src/App.js
@@ -1,12 +1,12 @@
+import "./App.css";
 import Navbar from "react-bootstrap/NavBar";
 import "material-icons/iconfont/material-icons.css";
-import "./App.css";
 import SearchBar from "./components/searchBar/SearchBar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar className="navbarHeader" fixed="top">
+      <Navbar className="navbarHeader" fixed="sticky" variant="dark">
         <Navbar.Brand href="">
           <span class="material-icons">privacy_tip</span>
           National Investigation Unit
--- Frontend/fp-group1/src/components/searchBar/SearchBar.jsx
+++ Frontend/fp-group1/src/components/searchBar/SearchBar.jsx
@@ -1,34 +1,37 @@
+import './SearchBar.css';
 import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
-import './SearchBar.css';
 
 const SearchBar = ({handleSearch, handleReset}) => {
 
     return (
         <>
-            <form id="searchBarForm" onSubmit={handleSearch}>
-                <Col md>
-                    <FloatingLabel controlId="floatingForenames" label="Forenames">
-                        <Form.Control type="text" name="searchForenames"/>
-                    </FloatingLabel>
-                </Col>
-                <Col md>
-                    <FloatingLabel controlId="floatingSurname" label="Surname">
-                        <Form.Control type="text" name="searchSurname"/>
-                    </FloatingLabel>
-                </Col>
-                <Col md>
-                    <FloatingLabel controlId="floatingGender" label="Gender">
-                        <Form.Select>
-                            <option value="Female"></option>
-                            <option value="Male"></option>
-                            <option value="Unknown"></option>
-                        </Form.Select>
-                    </FloatingLabel>
-                </Col>
+            <Form id="searchBarForm" onSubmit={handleSearch}>
+                <Row>
+                    <Col md>
+                        <FloatingLabel controlId="floatingForenames" label="Forenames">
+                            <Form.Control type="text" name="searchForenames"/>
+                        </FloatingLabel>
+                    </Col>
+                    <Col md>
+                        <FloatingLabel controlId="floatingSurname" label="Surname">
+                            <Form.Control type="text" name="searchSurname"/>
+                        </FloatingLabel>
+                    </Col>
+                    <Col md>
+                        <FloatingLabel controlId="floatingGender" label="Gender">
+                            <Form.Select>
+                                <option value="Female"></option>
+                                <option value="Male"></option>
+                                <option value="Unknown"></option>
+                            </Form.Select>
+                        </FloatingLabel>
+                    </Col>
+                </Row>
                 
-            </form>
+            </Form>
         </>
     )
 
