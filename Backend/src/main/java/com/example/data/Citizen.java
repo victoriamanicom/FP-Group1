d35d6e60851961b8ceb090120cae33c407e703a7
evaisnotonline <86105248+evaisnotonline@users.noreply.github.com>
2021-09-22 09:57:20 +0100

    FP1-5 Edited for MTO relationships - Citizen

--- Backend/src/main/java/com/example/data/Citizen.java
+++ Backend/src/main/java/com/example/data/Citizen.java
@@ -1,14 +1,8 @@
 package com.example.data;
 
-public class Citizen {
+import javax.persistence.Column;
 
-	private Long citizenID;
-	private String forenames;
-	private String surname;
-	private String homeAddress;
-	private String dateOfBirth;
-	private String placeOfBirth;
-	private String sex;
+public class Citizen {
 
 	public Citizen(String forenames, String surname, String homeAddress, String dateOfBirth, String placeOfBirth,
 			String sex) {
@@ -34,6 +28,27 @@ public class Citizen {
 		this.sex = sex;
 
 	}
+
+	@Column(name = "citizen_id")
+	private Long citizenID;
+
+	@Column(name = "forenames")
+	private String forenames;
+
+	@Column(name = "surname")
+	private String surname;
+
+	@Column(name = "home_address")
+	private String homeAddress;
+
+	@Column(name = "date_of_birth")
+	private String dateOfBirth;
+
+	@Column(name = "place_of_birth")
+	private String placeOfBirth;
+
+	@Column(name = "sex")
+	private String sex;
 
 	public Long getCitizenID() {
 		return citizenID;
@@ -89,6 +104,74 @@ public class Citizen {
 
 	public void setSex(String sex) {
 		this.sex = sex;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((citizenID == null) ? 0 : citizenID.hashCode());
+		result = prime * result + ((dateOfBirth == null) ? 0 : dateOfBirth.hashCode());
+		result = prime * result + ((forenames == null) ? 0 : forenames.hashCode());
+		result = prime * result + ((homeAddress == null) ? 0 : homeAddress.hashCode());
+		result = prime * result + ((placeOfBirth == null) ? 0 : placeOfBirth.hashCode());
+		result = prime * result + ((sex == null) ? 0 : sex.hashCode());
+		result = prime * result + ((surname == null) ? 0 : surname.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Citizen other = (Citizen) obj;
+		if (citizenID == null) {
+			if (other.citizenID != null)
+				return false;
+		} else if (!citizenID.equals(other.citizenID))
+			return false;
+		if (dateOfBirth == null) {
+			if (other.dateOfBirth != null)
+				return false;
+		} else if (!dateOfBirth.equals(other.dateOfBirth))
+			return false;
+		if (forenames == null) {
+			if (other.forenames != null)
+				return false;
+		} else if (!forenames.equals(other.forenames))
+			return false;
+		if (homeAddress == null) {
+			if (other.homeAddress != null)
+				return false;
+		} else if (!homeAddress.equals(other.homeAddress))
+			return false;
+		if (placeOfBirth == null) {
+			if (other.placeOfBirth != null)
+				return false;
+		} else if (!placeOfBirth.equals(other.placeOfBirth))
+			return false;
+		if (sex == null) {
+			if (other.sex != null)
+				return false;
+		} else if (!sex.equals(other.sex))
+			return false;
+		if (surname == null) {
+			if (other.surname != null)
+				return false;
+		} else if (!surname.equals(other.surname))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Citizen [citizenID=" + citizenID + ", forenames=" + forenames + ", surname=" + surname
+				+ ", homeAddress=" + homeAddress + ", dateOfBirth=" + dateOfBirth + ", placeOfBirth=" + placeOfBirth
+				+ ", sex=" + sex + "]";
 	}
 
 }
