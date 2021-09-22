14103e8453a1ab1498ae30b5d4ee4fa72ae18c4d
ivanokpo <ivanokpomor@gmail.com>
2021-09-22 10:58:48 +0100

    FP1-52 #Created ATMPoint Controller

---
+++ Backend/src/main/java/com/example/rest/ATMPointController.java
@@ -0,0 +1,14 @@
+package com.example.rest;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class ATMPointController {
+	
+	public ATMPointController() {
+		super();
+	}
+
+}
