2e6e3e7964f2fd5ea189ffeac2b9913e04d246d8
Seabohr <sebspace@hotmail.co.uk>
2021-09-22 16:23:21 +0100

    FP1-1 #set controlled states, just waiting for API address

--- Frontend/fp-group1/src/App.js
+++ Frontend/fp-group1/src/App.js
@@ -2,17 +2,45 @@ import "./App.css";
 import Navbar from "react-bootstrap/NavBar";
 import "material-icons/iconfont/material-icons.css";
 import SearchBar from "./components/searchBar/SearchBar";
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [suspectForenames, setSuspectForenames] = useState("");
+  const [suspectSurname, setSuspectSurname] = useState("");
+  const [suspectGender, setSuspectGender] = useState("");
+  const [citizens, setCitizens] = useState([]);
+
+  const handleReset = e => {
+    setSuspectForenames("");
+    setSuspectSurname("");
+    setSuspectGender("");
+
+  }
+
+  const handleSearch = e => {
+    e.preventDefault();
+    axios
+    .get()
+    .then((result) => {
+      setCitizens(result.data);
+    })
+    .catch(err => console.log(err));
+  }
+
   return (
     <div className="App">
       <Navbar className="navbarHeader" fixed="sticky" variant="dark">
         <Navbar.Brand href="">
-          <span class="material-icons">privacy_tip</span>
+          <span className="material-icons">privacy_tip</span>
           National Investigation Unit
         </Navbar.Brand>
       </Navbar>
-      <SearchBar />
+      <SearchBar handleReset={handleReset} handleSearch={handleSearch}
+      suspectForenames={suspectForenames} setSuspectForenames={setSuspectForenames}
+      suspectSurname={suspectSurname} setSuspectSurname={setSuspectSurname}
+      suspectGender={suspectGender} setSuspectGender={setSuspectGender}/>
     </div>
   );
 }
