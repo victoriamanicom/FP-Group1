b317f78da9d2c8696f07b8d26a6bf43f4a13b4e4
evaisnotonline <86105248+evaisnotonline@users.noreply.github.com>
2021-09-23 09:53:11 +0100

    FP1-5 Created CitizenDTO for Search tab

---
+++ Backend/src/main/java/com/example/rest/DTO/CitizenDTO.java
@@ -0,0 +1,106 @@
+package com.example.rest.DTO;
+
+public class CitizenDTO {
+
+	public CitizenDTO(String forenames, String surname, String homeAddress, String dateOfBirth) {
+		super();
+		this.forenames = forenames;
+		this.surname = surname;
+		this.homeAddress = homeAddress;
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public CitizenDTO() {
+		super();
+	}
+
+	private String forenames;
+
+	private String surname;
+
+	private String homeAddress;
+
+	private String dateOfBirth;
+
+	public String getForenames() {
+		return forenames;
+	}
+
+	public void setForenames(String forenames) {
+		this.forenames = forenames;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getHomeAddress() {
+		return homeAddress;
+	}
+
+	public void setHomeAddress(String homeAddress) {
+		this.homeAddress = homeAddress;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dateOfBirth == null) ? 0 : dateOfBirth.hashCode());
+		result = prime * result + ((forenames == null) ? 0 : forenames.hashCode());
+		result = prime * result + ((homeAddress == null) ? 0 : homeAddress.hashCode());
+		result = prime * result + ((surname == null) ? 0 : surname.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CitizenDTO other = (CitizenDTO) obj;
+		if (dateOfBirth == null) {
+			if (other.dateOfBirth != null)
+				return false;
+		} else if (!dateOfBirth.equals(other.dateOfBirth))
+			return false;
+		if (forenames == null) {
+			if (other.forenames != null)
+				return false;
+		} else if (!forenames.equals(other.forenames))
+			return false;
+		if (homeAddress == null) {
+			if (other.homeAddress != null)
+				return false;
+		} else if (!homeAddress.equals(other.homeAddress))
+			return false;
+		if (surname == null) {
+			if (other.surname != null)
+				return false;
+		} else if (!surname.equals(other.surname))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "CitizenDTO [forenames=" + forenames + ", surname=" + surname + ", homeAddress=" + homeAddress
+				+ ", dateOfBirth=" + dateOfBirth + "]";
+	}
+
+}
--- Backend/src/main/java/com/example/rest/DTO/DTO.java
+++
@@ -1,5 +0,0 @@
-package com.example.rest.DTO;
-
-public class DTO {
-
-}
