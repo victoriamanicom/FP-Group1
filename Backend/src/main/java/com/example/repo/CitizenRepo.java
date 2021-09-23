585ac41563067c603a2ea4cbb7e141f7fd42333f
evaisnotonline <86105248+evaisnotonline@users.noreply.github.com>
2021-09-23 19:26:43 +0100

    FP1-17 Created CitizenRepo

--- Backend/src/main/java/com/example/repo/CitizenRepo.java
+++ Backend/src/main/java/com/example/repo/CitizenRepo.java
@@ -1,5 +1,11 @@
 package com.example.repo;
 
-public class CitizenRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.rest.DTO.CitizenReturnDTO;
+
+@Repository
+public interface CitizenRepo extends JpaRepository<CitizenReturnDTO, Long> {
 
 }
