34156ee1a633cd78aabc73d72e21d9cb6aa01de8
evaisnotonline <86105248+evaisnotonline@users.noreply.github.com>
2021-09-23 19:27:40 +0100

    FP1-17 Created FinancialRepo

--- Backend/src/main/java/com/example/repo/FinancialRepo.java
+++ Backend/src/main/java/com/example/repo/FinancialRepo.java
@@ -1,5 +1,11 @@
 package com.example.repo;
 
-public class FinancialRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.rest.DTO.PeopleBankAccountDTO;
+
+@Repository
+public interface FinancialRepo extends JpaRepository<PeopleBankAccountDTO, Long> {
 
 }
