5546188413bca27b3329c31b91884703182994a5
Seabohr <sebspace@hotmail.co.uk>
2021-09-27 18:11:02 +0100

    FP1-19 #vehicle service made

--- Backend/src/main/java/com/example/service/VehicleService.java
+++ Backend/src/main/java/com/example/service/VehicleService.java
@@ -1,44 +1,51 @@
 package com.example.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import com.example.data.Citizen;
-import com.example.data.PeopleMobile;
 import com.example.data.VehicleRegistration;
 import com.example.repo.VehicleRepo;
-
 import com.example.rest.DTO.VehicleDTO;
 
 @Service
 public class VehicleService {
-	
+
 	private VehicleRepo vehicleRepo;
-	
-	
+
 	public VehicleService(VehicleRepo vehicleRepo) {
 		super();
 		this.vehicleRepo = vehicleRepo;
 	}
 
-	public VehicleRegistration findVehicle(VehicleRegistration vehicle) {
-		List<VehicleRegistration> peopleVehicle = this.vehicleRepo.findAll(Example.of(vehicle));
-		return null;
-		
-	}
-	
-	public VehicleDTO mapVToDTO(VehicleRegistration peoplevehicle) {
-			
-			
-			VehicleDTO dto = new VehicleDTO();
-			dto.setColour(peoplevehicle.getColour());
-			dto.setMake(peoplevehicle.getMake());
-			dto.setModel(peoplevehicle.getModel());
-			dto.setRegistrationDate(peoplevehicle.getRegistrationDate());
-			dto.setVehicleRegistrationNo(peoplevehicle.getVehicleRegistrationNo());
-			return dto;
-			
+	public List<VehicleDTO> findVehicles(Citizen citizen) {
+
+		VehicleRegistration citizenToVehicleRegistration = new VehicleRegistration();
+
+		citizenToVehicleRegistration.setForenames(citizen.getForenames());
+		citizenToVehicleRegistration.setSurname(citizen.getSurname());
+		citizenToVehicleRegistration.setDateOfBirth(citizen.getDateOfBirth());
+
+		List<VehicleRegistration> peopleVehicles = this.vehicleRepo.findAll(Example.of(citizenToVehicleRegistration));
+
+		ArrayList<VehicleDTO> suspectVehicles = new ArrayList<>();
+
+		for (VehicleRegistration pV : peopleVehicles) {
+
+			VehicleDTO suspectVehicleDTO = new VehicleDTO();
+
+			suspectVehicleDTO.setRegistrationDate(pV.getRegistrationDate());
+			suspectVehicleDTO.setMake(pV.getMake());
+			suspectVehicleDTO.setModel(pV.getModel());
+			suspectVehicleDTO.setColour(pV.getColour());
+			suspectVehicleDTO.setVehicleRegistrationNo(pV.getVehicleRegistrationNo());
+
+			suspectVehicles.add(suspectVehicleDTO);
 		}
+
+		return suspectVehicles;
+	}
 }
