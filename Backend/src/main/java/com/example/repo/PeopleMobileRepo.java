6140e9f18045c78b95c239bdb9405568f17d973e
evaisnotonline <86105248+evaisnotonline@users.noreply.github.com>
2021-09-29 09:36:58 +0100

    FP1-7 Removed DTO files from Repos

--- Backend/src/main/java/com/example/repo/PeopleMobileRepo.java
+++ Backend/src/main/java/com/example/repo/PeopleMobileRepo.java
@@ -8,14 +8,11 @@ import org.springframework.stereotype.Repository;
 import com.example.data.Citizen;
 import com.example.data.MobileCallRecords;
 import com.example.data.PeopleMobile;
-import com.example.rest.DTO.PeopleMobileDTO;
 
 @Repository
 public interface PeopleMobileRepo extends JpaRepository<PeopleMobile, Integer> {
 
 	PeopleMobile findPeopleMobile(Citizen citizen);
-
-	PeopleMobileDTO mapPMToDTO(PeopleMobile peoplemobile);
 
 	List<MobileCallRecords> findByReceiverMSISDN(String ReveiverMSISDN);
 
