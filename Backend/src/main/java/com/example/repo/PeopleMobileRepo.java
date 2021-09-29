01edc5d4ab3d44fee97584f1517574dc5f0b2398
evaisnotonline <86105248+evaisnotonline@users.noreply.github.com>
2021-09-29 09:48:16 +0100

    FP1-17 Deleted contents of all repo files

--- Backend/src/main/java/com/example/repo/PeopleMobileRepo.java
+++ Backend/src/main/java/com/example/repo/PeopleMobileRepo.java
@@ -1,19 +1,11 @@
 package com.example.repo;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.data.Citizen;
-import com.example.data.MobileCallRecords;
 import com.example.data.PeopleMobile;
 
 @Repository
 public interface PeopleMobileRepo extends JpaRepository<PeopleMobile, Integer> {
-
-	PeopleMobile findPeopleMobile(Citizen citizen);
-
-	List<MobileCallRecords> findByReceiverMSISDN(String ReveiverMSISDN);
 
 }
