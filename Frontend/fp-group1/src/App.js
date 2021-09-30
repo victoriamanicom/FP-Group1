1e2663dce269c1921bf3bcc76529d99bb427cd4b
Seabohr <sebspace@hotmail.co.uk>
2021-09-30 15:01:23 +0100

    FP1-1 #initial get data retrieval

--- Frontend/fp-group1/src/App.js
+++ Frontend/fp-group1/src/App.js
@@ -29,53 +29,20 @@ function App() {
     const handleSearch = (e) => {
         e.preventDefault();
 
-        // axios
-        // .get()
-        // .then(({ data }) => setCitizens(data))
-        // .catch((err) => console.log(err));
+        const searchCitizen = {
+            forenames: `${citizenForenames}`,
+            surname: `${citizenSurname}`,
+            sex: `${citizenGender}`
+        }
+        
 
-        setCitizens([
-            {
-                citizenID: "001L",
-                firstName: "First Name",
-                lastName: "Last Name 1",
-                dob: "DD/MM/YYYY",
-                sex: "Male",
-                Address: "5 Street Name",
-            },
-            {
-                citizenID: "001L",
-                firstName: "First Name",
-                lastName: "Last Name 2",
-                dob: "DD/MM/YYYY",
-                sex: "Male",
-                Address: "6 Street Name",
-            },
-            {
-                citizenID: "001L",
-                firstName: "First Name",
-                lastName: "Last Name 3",
-                dob: "DD/MM/YYYY",
-                sex: "Male",
-                Address: "7 Street Name",
-            },
-            {
-                citizenID: "001L",
-                firstName: "First Name",
-                lastName: "Last Name 4",
-                dob: "DD/MM/YYYY",
-                sex: "Male",
-                Address: "7 Street Name",
-            },
-            {
-                citizenID: "001L",
-                firstName: "First Name",
-                lastName: "Last Name 5",
-                dob: "DD/MM/YYYY",
-                sex: "Male",
-                Address: "7 Street Name",
-            },
-        ]);
+        console.log(searchCitizen);
+        axios
+        .post("http://54.72.172.119:5001/findCitizens", searchCitizen)
+        .then(({ data }) => setCitizens(data))
+        .catch((err) => console.log(err));
+
+
     };
 
     return (
