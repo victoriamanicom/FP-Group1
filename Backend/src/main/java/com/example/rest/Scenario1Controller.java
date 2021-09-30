a97a8966e5f9d397080a66ab4ab2abd9a9b750d4
evaisnotonline <86105248+evaisnotonline@users.noreply.github.com>
2021-09-30 14:53:45 +0100

    FP1-7 Get to Post

--- Backend/src/main/java/com/example/rest/Scenario1Controller.java
+++ Backend/src/main/java/com/example/rest/Scenario1Controller.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,7 +39,7 @@ public class Scenario1Controller {
 		this.mainservice = mainservice;
 	}
 
-	@GetMapping("/findCitizens")
+	@PostMapping("/findCitizens")
 	public ResponseEntity<List<CitizenReturnDTO>> findCitizens(@RequestBody Citizen citizen) {
 
 		List<CitizenReturnDTO> body = this.citizenservice.findCitizens(citizen);
